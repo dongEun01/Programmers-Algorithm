0c44a10cb48310e87b6e6a12d206d92871bf3425
KIM DONG EUN <121909239+dongEun01@users.noreply.github.com>
2025-05-13 23:58:57 +0900

    수정

---
+++ ProgrammersAlgorithm/src/CollectionFramework/collectionFramework.java
@@ -0,0 +1,27 @@
+package CollectionFramework;
+
+public class collectionFramework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		
+		1.자바의 STL 대응: 컬렉션 프레임워크
+		
+		<C++ STL>	                <Java 컬렉션 프레임워크>
+		vector	                ArrayList
+		list	                LinkedList
+		stack	                Stack, Deque
+		queue	                Queue, LinkedList, ArrayDeque
+		priority_queue	        PriorityQueue
+		map						HashMap, TreeMap, LinkedHashMap
+		set						HashSet, TreeSet, LinkedHashSet
+		unordered_map			HashMap
+		unordered_set			HashSet
+		
+		*/
+
+	}
+
+}
